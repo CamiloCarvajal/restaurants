c00a1f9b6f40348869bb72dc6ffab5f0f7a862c3
camilo carvajal <camilocc597@gmail.com>
2021-03-05 17:39:23 -0500

    icons

---
+++ navigations/RestaurantsStack.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {createStackNavigator} from '@react-navigation/stack'
+
+import Restaurants from '../screens/Restaurants'
+
+const Stack = createStackNavigator()
+
+export default function RestaurantsStack() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+                name="restaurant"
+                component={Restaurants}
+                options={{title:"Restaurantes"}}
+            ></Stack.Screen>
+        </Stack.Navigator>
+    )
+}

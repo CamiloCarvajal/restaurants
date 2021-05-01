5c6c0775e04748fd615f0ef4618c8e9cd8fc0495
Camilo Carvajal <camilo@talent.com>
2021-05-01 01:03:42 -0500

    adding map to restaurant detail

--- screens/Restaurants/Restaurant.js
+++ screens/Restaurants/Restaurant.js
@@ -1,10 +1,14 @@
+import { map } from "lodash";
 import { Dimensions } from "react-native";
 import React, { useState, useEffect } from "react";
-import { ScrollView, Alert, StyleSheet, Text, View } from "react-native";
 import CarouselImage from "../../components/CarouselImage";
+import { ScrollView, Alert, StyleSheet, Text, View } from "react-native";
 
 import Loading from "../../components/Loading";
+import { formatPhone } from "../../utils/helpers";
 import { getDocumentById } from "../../utils/action";
+import { ListItem, Rating, Icon } from "react-native-elements";
+import MapRestaurant from "../../components/restaurants/MapRestaurant";
 
 const widthScreen = Dimensions.get("window").width;
 
@@ -42,9 +46,102 @@ export default function Restaurant({ navigation, route }) {
         activeSlide={activeSlide}
         setActiveSlide={setActiveSlide}
       />
-      <Text>{restaurant.description}</Text>
+      <TitleRestaurant
+        name={restaurant.name}
+        description={restaurant.description}
+        rating={restaurant.rating}
+      />
+      <RestauranInfo
+        name={restaurant.name}
+        location={restaurant.location}
+        address={restaurant.address}
+        email={restaurant.email}
+        phone={formatPhone(restaurant.phone)}
+      />
     </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({ viewBody: { flex: 1 } });
+function TitleRestaurant({ name, description, rating }) {
+  return (
+    <View style={styles.viewRestaurantTitle}>
+      <View style={styles.viewRestaurantContainer}>
+        <Text style={styles.nameRestaurant}>{name}</Text>
+        <Rating
+          style={styles.rating}
+          imageSize={20}
+          startingValue={parseFloat(rating)}
+          readonly
+        />
+      </View>
+      <Text style={styles.descriptionRestaurant}>{description}</Text>
+    </View>
+  );
+}
+
+function RestauranInfo({ name, location, address, email, phone }) {
+  const listInfo = [
+    { text: address, iconName: "map-marker" },
+    { text: phone, iconName: "phone" },
+    { text: email, iconName: "at" },
+  ];
+
+  return (
+    <View style={styles.viewRestaurantInfo}>
+      <Text style={styles.restaurantInfoTitle}>
+        Informacion sobre el restaurante
+      </Text>
+      <MapRestaurant location={location} name={name} height={150} />
+      {map(listInfo, (item, index) => (
+        <ListItem key={index} style={styles.containerListItem}>
+          <Icon
+            type="material-community"
+            name={item.iconName}
+            color="#442484"
+          />
+          <ListItem.Content>
+            <ListItem.Title>{item.text}</ListItem.Title>
+          </ListItem.Content>
+        </ListItem>
+      ))}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  viewBody: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  viewRestaurantTitle: {
+    padding: 15,
+  },
+  viewRestaurantContainer: {
+    flexDirection: "row",
+  },
+  nameRestaurant: {
+    fontWeight: "bold",
+  },
+  descriptionRestaurant: {
+    marginTop: 8,
+    color: "gray",
+    textAlign: "justify",
+  },
+  rating: {
+    position: "absolute",
+    right: 0,
+  },
+  viewRestaurantInfo: {
+    margin: 15,
+    marginTop: 25,
+  },
+  restaurantInfoTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 15,
+  },
+  containerListItem: {
+    borderBottomColor: "#a375c2",
+    borderBottomWidth: 1,
+  },
+});
